2d395dbf27d2b97de763fda4dce1f7fd6e8bc2f2
Aditya Kumar <aditya157451@gmail.com>
2026-09-11 10:06:06 +0530

    changes

--- main.py
+++ main.py
@@ -78,4 +78,4 @@ app.include_router(report_router)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get('PORT', 5003)))
+    uvicorn.run("main:app", host="0.0.0.0", port=9000, reload=True)
